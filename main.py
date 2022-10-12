4d16b7b378e284dfa4a92f882a809adefca96138
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-10-12 18:51:41 +0530

    now we can run the file also from terminal itself

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
+import sys
 import src.basic as basic
 
-while True:
-    try:
-        text = input("JAK_Programming_Language>>> ")
-        if text == "exit()":
+if sys.argv:
+    file_name = sys.argv[1]
+    result, error = basic.run("<stdin>", f"RUN(\"{file_name}\")")
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
+    print()
+else:
+    while True:
+        try:
+            text = input("JAK_Programming_Language>>> ")
+            if text == "exit()":
+                break
+            if text.strip() == "":
+                continue
+            result, error = basic.run("<stdin>", text)
+            if error:
+                print(error.as_string())
+            elif result:
+                if len(result.elements) == 1:
+                    print(repr(result.elements[0]))
+                else:
+                    print(repr(result))
+            print()
+        except Exception as e:
+            print(f"Exception: {e}")
+        except KeyboardInterrupt:
             break
-        if text.strip() == "":
-            continue
-        result, error = basic.run("<stdin>", text)
-        if error:
-            print(error.as_string())
-        elif result:
-            if len(result.elements) == 1:
-                print(repr(result.elements[0]))
-            else:
-                print(repr(result))
-        print()
-    except Exception as e:
-        print(f"Exception: {e}")
-    except KeyboardInterrupt:
-        break
