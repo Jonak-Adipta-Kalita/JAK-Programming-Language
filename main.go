20e214a00ee7ef9ba685b1e55f557e3d88b702e5
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-06 15:24:20 +0530

    reading the code!!

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print(">>> ")
+	text, _ := reader.ReadString('\n')
+	fmt.Print(text)
 }
