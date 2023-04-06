0c72ff5b1d69d69c4c3c7858414090b2a89f8285
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-06 15:55:01 +0530

    imma confused

--- main.go
+++ main.go
@@ -13,3 +13,20 @@ func main() {
 	text, _ := reader.ReadString('\n')
 	fmt.Print(text)
 }
+
+// lexer
+// 1. read the input
+// 2. break it into tokens
+// 3. return the tokens one at a time
+
+// parser
+// 1. read the tokens
+// 2. determine if the syntax is valid
+// 3. if valid, return the AST
+// 4. if invalid, return an error
+
+// AST
+// 1. the AST is just a tree of nodes
+// 2. each node is an expression
+// 3. each expression has a type
+// 4. each expression has zero or more children
