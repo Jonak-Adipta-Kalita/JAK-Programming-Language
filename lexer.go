cd45fa69a2a22ae5ed1ca6d2a5f0249dc5cd6173
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-06 22:32:35 +0530

    alright

--- lexer.go
+++ lexer.go
@@ -1,1 +1,15 @@
 package main
+
+func isWhiteSpace(char byte) bool {
+	return char == ' ' || char == '\t' || char == '\n'
+}
+
+func isDigit(char byte) bool {
+	return char >= '0' && char <= '9'
+}
+
+func isLetter(char byte) bool {
+	return char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' || char == '_'
+}
+
+var eof = rune(0)
