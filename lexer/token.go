2a2ffb64a2bcc1afb45e8e33239e76d9ed28d4c7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-07 12:34:47 +0530

    maybe i can do it myself? :pray:

--- lexer/token.go
+++ lexer/token.go
@@ -5,25 +5,15 @@ type TokenType uint
 const (
 	TT_EOF TokenType = iota
 	TT_ILLEGAL
-	TT_WHITE_SPACE
 	TT_PLUS
 	TT_MINUS
 	TT_MUL
 	TT_DIV
 	TT_POW
-	TT_ASSIGN
-	TT_KEYWORD
-	TT_INT
-	TT_STRING
-	TT_SEMICOLON
+	TT_EQUAL
 	TT_LPAREN
 	TT_RPAREN
-	TT_NEWLINE
 )
-
-var KEYWORDS = []string{
-	"print",
-}
 
 type Token struct {
 	Value string
