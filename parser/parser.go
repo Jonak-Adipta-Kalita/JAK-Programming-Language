f56a1a95bdfd55398c5b0069bd5ba9d205c3d7b3
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-07 21:14:41 +0530

    `gofmt` is not working....

--- parser/parser.go
+++ parser/parser.go
@@ -46,8 +46,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.nextToken()
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
-p.registerPrefix(token.IDENTIFIER, p.parseIdentifier)
-p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.IDENTIFIER, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	return p
 }
@@ -180,11 +180,13 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+
 	if err != nil {
-	msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
-	p.errors = append(p.errors, msg)
-	return nil
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
 	}
+
 	lit.Value = value
 	return lit
-	}
+}
