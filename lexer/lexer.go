ac50f8e9ee1fb1331939b476ef0f36ddb248587f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-07 12:22:36 +0530

    helper functions!!

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,13 @@
 package lexer
 
-func NewLexer() {
+func isWhiteSpace(char rune) bool {
+	return char == ' ' || char == '\t' || char == '\n'
+}
 
+func isDigit(char rune) bool {
+	return '0' <= char && char <= '9'
+}
+
+func isLetter(char rune) bool {
+	return ('a' <= char && char <= 'z') || ('A' <= char && char <= 'Z') || char == '_'
 }
