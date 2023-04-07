3c5856edf8f0c7bbf84a5f9db2fc41ed6e74312b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-07 10:47:34 +0530

    lets update go version

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fileName := os.Args[1]
+	
+	if fileName == "" {
+		for {
+			var codeLine string
+			fmt.Print(">>> ")
+			fmt.Scanln(&codeLine)
+
+			// TODO: Run code
+		}
+	}
+
 	file, err := os.Open(fileName)
 
 	if err != nil {
