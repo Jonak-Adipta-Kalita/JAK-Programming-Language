6b84f18fe9900ba98883a9004d6baa4fe3f51759
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-08 22:04:39 +0530

    string concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -163,6 +163,8 @@ func evalInfixExpression(
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s",
 			left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s",
 			left.Type(), operator, right.Type())
@@ -222,6 +224,19 @@ func evalIntegerInfixExpression(
 			left.Type(), operator, right.Type())
 
 	}
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s",
+			left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
