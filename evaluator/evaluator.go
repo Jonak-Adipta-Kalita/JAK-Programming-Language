392ebb4fcb8ac1e5a452619ba11381af859e9cde
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-08 14:17:27 +0530

    boolean evaluated...

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/Jonak-Adipta-Kalita/JAK-Programming-Language/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -14,13 +19,23 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	return nil
 }
+
 func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 	for _, statement := range stmts {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
