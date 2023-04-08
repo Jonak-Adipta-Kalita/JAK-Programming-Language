aeb9a2995557df7960c429a26ffaed6fab4b2e30
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-08 14:59:55 +0530

    solved error in parsing for if else....

--- parser/parser.go
+++ parser/parser.go
@@ -134,10 +134,22 @@ func (p *Parser) parseBoolean() ast.Expression {
 
 func (p *Parser) parseIfExpression() ast.Expression {
 	expression := &ast.IfExpression{Token: p.curToken}
+
 	if !p.expectPeek(token.LPAREN) {
 		return nil
 	}
+
 	p.nextToken()
+
+	expression.Condition = p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
 	expression.Consequence = p.parseBlockStatement()
 	if p.peekTokenIs(token.ELSE) {
 		p.nextToken()
@@ -146,6 +158,7 @@ func (p *Parser) parseIfExpression() ast.Expression {
 		}
 		expression.Alternative = p.parseBlockStatement()
 	}
+
 	return expression
 }
 
