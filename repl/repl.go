3f3d355d88782bb39da41ca3505afe3b8638f7bd
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-08 12:45:43 +0530

    yeah i dont even understand whats happening at this point!!

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/Jonak-Adipta-Kalita/JAK-Programming-Language/lexer"
-	"github.com/Jonak-Adipta-Kalita/JAK-Programming-Language/token"
+	"github.com/Jonak-Adipta-Kalita/JAK-Programming-Language/parser"
 )
 
 const PROMPT = ">>> "
@@ -21,8 +21,19 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
