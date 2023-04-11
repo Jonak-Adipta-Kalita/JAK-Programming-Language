955f064a6639e4f8e277129ec75023521cce982b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-04-11 22:33:31 +0530

    detach function for arrays

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -7,23 +7,6 @@ import (
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": {
-		Fn: func(file string, line int, args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", file, line,
-					len(args))
-			}
-			switch arg := args[0].(type) {
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("argument to `len` not supported, got %s", file, line,
-					args[0].Type())
-			}
-		},
-	},
 	"first": {
 		Fn: func(file string, line int, args ...object.Object) object.Object {
 			if len(args) != 1 {
@@ -59,7 +42,7 @@ var builtins = map[string]*object.Builtin{
 			return NULL
 		},
 	},
-	"push": {
+	"append": {
 		Fn: func(file string, line int, args ...object.Object) object.Object {
 			if len(args) != 2 {
 				return newError("wrong number of arguments. got=%d, want=2", file, line,
@@ -75,6 +58,46 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"detach": {
+		Fn: func(file string, line int, args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", file, line,
+					len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `detach` must be ARRAY, got %s", file, line,
+					args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			index := args[1].(*object.Integer).Value
+
+			length := len(arr.Elements)
+			if int(index) >= length {
+				return newError("index out of range", file, line)
+			}
+			newElements := make([]object.Object, length-1)
+			copy(newElements[:index], arr.Elements[:index])
+			copy(newElements[index:], arr.Elements[index+1:])
+			return &object.Array{Elements: newElements}
+		},
+	},
+	"len": {
+		Fn: func(file string, line int, args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", file, line,
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s", file, line,
+					args[0].Type())
+			}
 		},
 	},
 	"print": {
