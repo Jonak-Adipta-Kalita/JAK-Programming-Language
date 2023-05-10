5b4647c5b7474c98f1fceec7c464f9018dd5c958
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-05-10 08:32:01 +0530

    the current error handling system is too messed up

--- object/errors.go
+++ object/errors.go
@@ -3,16 +3,17 @@ package object
 import "fmt"
 
 type Error struct {
-	Message  string
-	File     string
-	Line     int
-	StartPos int
-	EndPos   int
+	Message   string
+	File      string
+	ErrorName string
+	Line      int
+	StartPos  int
+	EndPos    int
 }
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string {
-	message := fmt.Sprintf("Error: %s", e.Message)
+	message := fmt.Sprintf("Error: `%s`", e.Message)
 	message += fmt.Sprintf("\n\tat %s: %d", e.File, e.Line+1)
 
 	return message
