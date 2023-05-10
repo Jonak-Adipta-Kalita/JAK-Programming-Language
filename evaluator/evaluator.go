059e5d13e5988d65e45660da959f200241c08904
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2023-05-10 08:28:43 +0530

    a base for errors!

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -549,11 +549,8 @@ func isTruthy(obj object.Object) bool {
 }
 
 func newError(format, file string, line int, a ...interface{}) *object.Error {
-	if file == "<stdin>" {
-		return &object.Error{Message: fmt.Sprintf(format, a...)}
-	}
 	args := append([]interface{}{file, line}, a...)
-	return &object.Error{Message: fmt.Sprintf("File: %s: Line: %d: "+format, args...)}
+	return &object.Error{Message: fmt.Sprintf(format, args...), File: file, Line: line}
 }
 
 func PrintParserErrors(out io.Writer, errors []string) {
--- object/errors.go
+++ object/errors.go
@@ -1,5 +1,7 @@
 package object
 
+import "fmt"
+
 type Error struct {
 	Message  string
 	File     string
@@ -9,7 +11,12 @@ type Error struct {
 }
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
-func (e *Error) Inspect() string  { return e.Message }
+func (e *Error) Inspect() string {
+	message := fmt.Sprintf("Error: %s", e.Message)
+	message += fmt.Sprintf("\n\tat %s: %d", e.File, e.Line+1)
+
+	return message
+}
 func (e *Error) InvokeMethod(method string, args ...Object) Object {
 	return nil
 }
